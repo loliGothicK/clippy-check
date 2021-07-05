144c15a693002fa94e228588f8d8ad2ecebc8434
Mitama <loligothick@gmail.com>
2021-07-05 23:58:52 +0900

    v0.1.1

--- src/input.ts
+++ src/input.ts
@@ -15,16 +15,11 @@ export interface Input {
 export function get(): Input {
     const token = core.getInput('token', { required: true });
     const options = stringArgv(core.getInput('options', { required: false }));
-    const warn = core.getMultilineInput('warn', { required: false });
-    const allow = core.getMultilineInput('allow', { required: false });
-    const deny = core.getMultilineInput('deny', { required: false });
-    const forbid = core.getMultilineInput('forbid', { required: false });
+    const warn = stringArgv(core.getInput('warn', { required: false }));
+    const allow = stringArgv(core.getInput('allow', { required: false }));
+    const deny = stringArgv(core.getInput('deny', { required: false }));
+    const forbid = stringArgv(core.getInput('forbid', { required: false }));
     const name = core.getInput('name', { required: false });
-
-    core.setOutput('warn', `${warn}`);
-    core.setOutput('allow', `${allow}`);
-    core.setOutput('deny', `${deny}`);
-    core.setOutput('forbid', `${forbid}`);
 
     return {
         token,
